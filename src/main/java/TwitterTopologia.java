f6e858a3eb543d3a7e782bb7a744e1eb77d8f9d9
antonioalcocer13 <antonioalcocer13@gmail.com>
2016-04-21 20:57:40 +0200

    paso4

--- src/main/java/TwitterTopologia.java
+++ src/main/java/TwitterTopologia.java
@@ -1,20 +1,26 @@
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
+import backtype.storm.stats.RollingWindow;
 import backtype.storm.topology.TopologyBuilder;
 import bolt.FileWriterBolt;
 import bolt.HashtagExtractionBolt;
 import bolt.LanguageDetectionBolt;
+import bolt.RollingCountBolt;
 import spout.TwitterSpout;
 import twitter4j.FilterQuery;
 
 
 public class TwitterTopologia {
 
-    private static String consumerKey = "FILL IN HERE";
-    private static String consumerSecret = "FILL IN HERE";
-    private static String accessToken = "FILL IN HERE";
-    private static String accessTokenSecret = "FILL IN HERE";
+//    private static String consumerKey = "FILL IN HERE";
+//    private static String consumerSecret = "FILL IN HERE";
+//    private static String accessToken = "FILL IN HERE";
+//    private static String accessTokenSecret = "FILL IN HERE";
+    private static String consumerKey = "rR7yrSFfb05Dj7sfF6J57dz0G";
+    private static String consumerSecret = "8IYdN3VuzqtBQx9VF67MAUzlFLboOlteILYXocuH3Z3NRlxPZB";
+    private static String accessToken = "176882080-xl7GJfkdGPO74iIHGVut9Hk8Z6ScxlLZy7pdHOBQ";
+    private static String accessTokenSecret = "7uJSrDS2OIDTBWnGZwz0p4J6RAdT6FmbhTJztjFAtL35k";
 
 
 
@@ -47,16 +53,19 @@ public class TwitterTopologia {
         TwitterSpout spout = new TwitterSpout(consumerKey, consumerSecret, accessToken, accessTokenSecret, tweetFilterQuery);
         //TODO: Set the twitter spout as spout on this topology. Hint: Use the builder object.
 
-        FileWriterBolt fileWriterBolt = new FileWriterBolt("hashtag.txt");
+        FileWriterBolt fileWriterBolt = new FileWriterBolt("contador.txt");
         //TODO: Route messages from the spout to the file writer bolt. Hint: Again, use the builder object.
 
         LanguageDetectionBolt lenguaje= new LanguageDetectionBolt();
         HashtagExtractionBolt hashtag = new HashtagExtractionBolt();
+        RollingCountBolt contador = new RollingCountBolt(9, 3);
 
         builder.setSpout("spoutLeerTwitter",spout,1);
         builder.setBolt("lenguaje",lenguaje,1).shuffleGrouping("spoutLeerTwitter");
         builder.setBolt("hashtag",hashtag,1).shuffleGrouping("lenguaje");
-        builder.setBolt("escribirFichero",fileWriterBolt,1).shuffleGrouping("hashtag");
+        builder.setBolt("cont",contador,1).shuffleGrouping("hashtag");
+        builder.setBolt("escribirFichero",fileWriterBolt,1).shuffleGrouping("cont");
+
 
         Config conf = new Config();
         conf.setDebug(true);
